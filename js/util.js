1130828c1985ced5334bf464a06204bb741afa38
Waranim <129619485+Waranim@users.noreply.github.com>
2023-10-23 18:21:03 +0500

    Задание 5.14

---
+++ js/util.js
@@ -0,0 +1,17 @@
+const generateRandomNumber = (min, max) => {
+  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+
+  return Math.floor(result);
+};
+
+const shuffle = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
+
+export {generateRandomNumber, shuffle};
